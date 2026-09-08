b05ff1e362d86cb23600ae5b6fba1340d667b622
Anup Patwa <anuppatwa1090@gmail.com>
2026-09-08 16:58:00 +0530

    Delete 10todocontextLocal directory

--- 10todocontextLocal/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import TodoForm from "./TodoForm";
-import TodoItem from "./TodoItem";
-import TodoFilter from "./TodoFilter";
-import TodoStats from "./TodoStats";
-
-export { TodoForm, TodoItem, TodoFilter, TodoStats };
