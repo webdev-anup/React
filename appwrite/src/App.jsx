4dd85dfdedf2cde140362b055cdf9e1c4dda5c70
Your Name <your-email@example.com>
2026-09-17 01:24:17 +0530

    Add new components and update layout

--- appwrite/src/App.jsx
+++ appwrite/src/App.jsx
@@ -1,13 +1,39 @@
+import React, {useState, useEffect} from 'react'
+import {dispatch} from 'react-redux'
 import "./App.css";
+import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice"
+import Header from './components/header/header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A Blog App in Appwrite</h1>
-    </>
-  );
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          Todo: <outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App;
